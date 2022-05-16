391ece885338b83e03a263d17f47525f8f3a2626
adityaiiitr <adityaiiitr@gmail.com>
2022-05-16 22:56:11 +0530

    Largest Sorted Array Length Corrected!

--- largest_sorted_subarray.c
+++ largest_sorted_subarray.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 int main()
 {
-    int n, count = 0, max_count = 0;
+    int n, count = 1, max_count = 0;
     printf("Enter The size of array: ");
     scanf("%d", &n);
     int arr[n];
     int start = 0, end = n - 1;
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
-    for (int i = 0; i < n - 1; i++)
+    int i;
+    for (i = 0; i < n - 1; i++)
     {
         if (arr[i] < arr[i + 1])
         {
+            count++;
+        }
+        else
+        {
+            if (count > max_count)
+                max_count = count;
+            count = 1;
         }
     }
-
-    printf("Sorted Subarray Length %d.", max_count);
+    if (count > max_count)
+        max_count = count;
+    if (i != n + 1)
+        printf("Sorted Subarray Length %d.", max_count);
+    else
+        printf("Sorted Subarray Length %d.", (max_count + 1));
 
     return 0;
 }
